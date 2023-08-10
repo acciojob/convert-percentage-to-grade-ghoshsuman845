87e2dfe14aa4130c677d323cb036e8b4a6282434
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-11 04:30:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+//your code here
 function calculateGrade(percentage) {
-  //your code here
+  if (percentage >= 90) {
+    return "A";
+  } else if (percentage >= 80) {
+    return "B";
+  } else if (percentage >= 70) {
+    return "C";
+  } else if (percentage >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
